9356d00db66848dc9a9cc57785511139f906ff9c
M Adnan Khan <160040993+Adnan00Khan@users.noreply.github.com>
2024-11-10 06:53:52 +0500

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,71 @@
+
 import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="box">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <div className="nav">
+        <br />
+        <Image src="/img/img.jpg" alt="" width={250} height={250} className="img" />
+        <hr />
+       <a href="#about"><button className="nav-btn">about</button></a> 
+        <hr />
+        <a href="#education"><button className="nav-btn">education</button></a>
+        <hr />
+        <a href="#skills"><button className="nav-btn">skill</button></a>
+        <hr />
+        <a href="#project"><button className="nav-btn">projects</button></a>
+      </div>
+
+
+      <div className="box1">
+
+      <div className="main" id="about">
+        <br />
+        <h1 className="about-h1">adnan khan</h1>
+        <p className="about-p">Email: "adyyy931@gmail.com" Linkedin: <a className="about-a" href="https://www.linkedin.com/in/adnan-khan-14b0aa25b/">ADNAN KHAN</a></p>
+        <br />
+        <p className="about-p">Hi, AADNAN KHAN, I specialize in front-end and back-end development, bringing ideas to life through clean, efficient, and user-friendly code. Whether it's designing visually appealing interfaces or ensuring seamless functionality, I thrive on creating solutions that meet both user and business needs. </p>
+        <button className="about-btn"><a className="a" href="https://hackathon-1-2-one.vercel.app/">CV</a></button>
+      </div>
+<hr />
+      <div className="main" id="education">
+        <br />
+       <ul className="education-ul">
+        <li className="education-li">SSC (Secondary School Certificate)</li>
+        <li className="education-li">HSC (Higher Secondary Certificate)</li>
+        <li className="education-li">ADIT(Advanced Diploma In Information Technology)</li>
+       </ul>
+      </div>
+<hr />
+<div className="main" id="skills">
+        <br />
+       <ul className="education-ul">
+       <li className="education-li">ms office</li>
+        <li className="education-li">html</li>
+        <li className="education-li">css</li>
+        <li className="education-li">tailwind </li>
+        <li className="education-li">javascript</li>
+        <li className="education-li">typescript</li>
+        <li className="education-li">nextjs</li>
+        <li className="education-li">nodejs</li>
+       </ul>
+      </div>
+<hr />
+<div className="main" id="project">
+        <br />
+       <ul className="education-ul">
+        <li className="education-li">static web ux/ui</li><a className="a" href="https://coffee-shop-livid-seven.vercel.app/">Go</a>
+        <li className="education-li">resume builder</li><a className="a" href="https://hackthon-3-4.vercel.app/">Go</a>
+        <li className="education-li">multipage web static</li><a className="a" href="https://multi-page-web-css.vercel.app/">Go</a>
+       </ul>
+      </div>
+
+
+      </div>
+      </div>
+    
   );
 }
